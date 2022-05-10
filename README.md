e7f2536fd337fbf9e4850d15d4fed6b7342aa1bb
Edgar Griffin <edgarcgriffinjr@gmail.com>
2022-05-09 23:16:35 -0400

    second commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Welcome
+
+This is possibly the simplest thing I've ever done. This is a placeholder for a new portfolio site that I develop by end of June. 
+
--- index.js
+++
@@ -1,9 +0,0 @@
-function swapMode() {
-    if (document.getElementById('checkMode').className = 'dark') {
-        document.getElementById('checkMode').className = 'light'
-    } else if (document.getElementById('checkMode').className = 'light'){
-        document.getElementById('checkMode').className = 'dark'
-    }
-
-    console.log(document.getElementById('checkMode').className)
-}
